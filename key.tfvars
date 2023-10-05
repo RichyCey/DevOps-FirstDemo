a105da88346d4b022220dd8871ecf0e1d937d3ed
RichyCey <alltair.ua@gmail.com>
2023-10-05 13:17:08 +0300

    Terraform

--- key.tfvars
+++ key.tfvars
@@ -1,2 +1,2 @@
-#aws_access_key = local.terraform_keys.terraform_access_key
-#aws_secret_key = local.terraform_keys.terraform_secret_access_key
+aws_access_key = local.terraform_keys.terraform_access_key
+aws_secret_key = local.terraform_keys.terraform_secret_access_key
--- provider.tf
+++ provider.tf
@@ -11,32 +11,11 @@ terraform {
   }
 }
 
-# Define an AWS Secrets Manager Secret
-resource "aws_secretsmanager_secret" "my_secret" {
-  name = "terraform_keys"  # Replace with the actual name or ARN of your secret
-}
-
-# Define an AWS Secrets Manager Secret Version
-data "aws_secretsmanager_secret_version" "creds" {
-  secret_id = aws_secretsmanager_secret.my_secret.id
-}
-
-# Access the credentials using locals or variables
-locals {
-  aws_access_key = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["access_key"]
-  aws_secret_access_key = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["secret_access_key"]
-}
-
-# Explicitly define the dependency order to prevent a cycle
-resource "null_resource" "dependency" {
-  depends_on = [aws_secretsmanager_secret.my_secret]
-}
-
 # configuring docker and aws as providers
 provider "docker" {}
 
 provider "aws" {
   region     = var.aws_region
-  access_key = local.aws_access_key
-  secret_key = local.aws_secret_access_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
