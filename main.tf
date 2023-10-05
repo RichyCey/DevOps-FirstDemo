76624ff2ab9a3ed50dfde54cf39ad5dece478fe9
RichyCey <alltair.ua@gmail.com>
2023-10-05 15:19:34 +0300

    Jenkins

--- main.tf
+++ main.tf
@@ -1,52 +1,79 @@
-############CREATING A ECS CLUSTER#############
+# Provider definition
+provider "aws" {
+  region = "us-east-1"
+}
 
-resource "aws_ecs_cluster" "cluster" {
-  name = "cluster"
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
+# VPC definition
+data "aws_vpc" "existing" {
+  id = aws_vpc.main.id  # Replace "vpc-XXXXXXXXXXXXXXXXX" with your VPC ID
+}
+
+# Security group for the ECS tasks
+resource "aws_security_group" "ecs_sg" {
+  vpc_id = data.aws_vpc.existing.id
+  name   = "ecs-security-group"
+  # Inbound and outbound rules
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_ecs_task_definition" "task" {
-  family                   = "service"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE", "EC2"]
-  cpu                      = 512
-  memory                   = 2048
-  container_definitions    = <<DEFINITION
-  [
+# ECS task definition
+resource "aws_ecs_task_definition" "task_definition" {
+  family                = "miniflask-api-task"
+  network_mode          = "awsvpc"
+  memory                = "512"
+  requires_compatibilities = ["FARGATE"]
+
+  # Task execution role (Replace "XXX" with your IAM role ARN)
+  execution_role_arn    = "arn:aws:iam::584934534486:role/ecsRole"  # Replace "XXX" with your IAM role ARN
+
+  # Container definition
+  container_definitions = jsonencode([
     {
-      "name"      : "nginx",
-      "image"     : "nginx:1.23.1",
-      "cpu"       : 512,
-      "memory"    : 2048,
-      "essential" : true,
-      "portMappings" : [
+      name      = "miniflask-api-container"
+      image     = "public.ecr.aws/g1s5q2a7/miniflask-api:latest" 
+      cpu       = 256
+      memory    = 512
+      port_mappings = [
         {
-          "containerPort" : 80,
-          "hostPort"      : 80
+          container_port = 5000
+          host_port      = 5000
+          protocol       = "tcp"
         }
       ]
     }
-  ]
-  DEFINITION
+  ])
+
+  # Defining the task-level CPU
+  cpu = "256"  
+}
+
+# ECS service
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "minimal-api-cluster"  
 }
 
 resource "aws_ecs_service" "service" {
-  name             = "service"
-  cluster          = aws_ecs_cluster.cluster.id
-  task_definition  = aws_ecs_task_definition.task.id
-  desired_count    = 1
-  launch_type      = "FARGATE"
-  platform_version = "LATEST"
+  name            = "miniflask-api-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task_definition.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
 
+  # Network configuration
   network_configuration {
+    subnets          = [aws_subnet.subnet.id]  # Replace "subnet-XXX" with your subnet IDs
+    security_groups  = [aws_security_group.ecs_sg.id]
     assign_public_ip = true
-    security_groups  = [aws_security_group.sg.id]
-    subnets          = [aws_subnet.subnet.id]
-  }
-  lifecycle {
-    ignore_changes = [task_definition]
   }
 }
